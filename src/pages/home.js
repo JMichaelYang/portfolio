e0246924991f5791ba72e523dd33ad57646c1c2b
jaewon <jaewonmichael@gmail.com>
2022-04-04 02:50:35 -0400

    background image cover

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
 
 class Home extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Welcome to Jaewon's Portfolio</h1>
-        <img src={process.env.PUBLIC_URL + '/static/images/portfolio_cartoon.png'} alt='home page background'/>
-      </div>
+      <Background image_url={process.env.PUBLIC_URL + '/static/images/portfolio_cartoon.png'}>
+        <div>Welcome to Jaewon's Portfolio</div>
+      </Background>
     );
   }
 }
+
+const Background = styled.div`
+  background-image: url(${props => props.image_url});
+  background-size: cover;
+  width: 100%;
+  height: 100%;
+`;
 
 export default Home;
