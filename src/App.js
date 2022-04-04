92cc26414d097a709f1f4d727cac306e067fd420
jaewon <jaewonmichael@gmail.com>
2022-04-04 02:07:47 -0400

    moved images

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  // TODO: Update 404 path to a 404 page.
+  render() {
+    return (
+      <Router>
+        <Routes>
+          <Route path='/'>
+            <Home/>
+          </Route>
+          <Route path='*'>
+            <Home/>
+          </Route>
+        </Routes>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/pages/home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import background from '../../static/images/portfolio_background.png';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome to Jaewon's Portfolio</h1>
+        <img src={background}/>
+      </div>
+    );
+  }
+}
+
+export default Home;
