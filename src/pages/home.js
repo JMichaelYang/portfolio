540c997e23a891e8f1f1690f27df024dedc945aa
jaewon <jaewonmichael@gmail.com>
2022-04-04 02:38:15 -0400

    updated image paths

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import background from '../static/images/portfolio_cartoon.png';
 
 class Home extends React.Component {
   render() {
     return (
       <div>
         <h1>Welcome to Jaewon's Portfolio</h1>
-        <img src={background} alt='home page background'/>
+        <img src={process.env.PUBLIC_URL + '/static/images/portfolio_cartoon.png'} alt='home page background'/>
       </div>
     );
   }
